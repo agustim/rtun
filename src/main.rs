6e9170234f3eac39433c8b5c352e3b0cc389fdec
Agustí Moll <agusti@gmail.com>
2024-05-01 22:07:45 +0200

    send info

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use tokio::net::UdpSocket;
 use std::net::{IpAddr, SocketAddr};
 use std::error::Error;
 use std::str::FromStr;
-use std::io::{stdin, Read};
+use std::io::stdin;
 
 
 const UDP_BUFFER_SIZE: usize = 17480; // 17kb
@@ -45,7 +45,7 @@ impl Server {
             if let Some((size, peer)) = to_send {
                 let amt = socket.send_to(&buf[..size], &peer).await?;
 
-                println!("Echoed {}/{} bytes to {}", amt, size, peer);
+                print!("Echoed {}/{} bytes to {} -> {}", amt, size, peer, String::from_utf8_lossy(&buf[..size]));
             }
 
             to_send = Some(socket.recv_from(&mut buf).await?);
@@ -70,8 +70,9 @@ async fn server_mode(port: u16) -> Result<(), Box<dyn Error>> {
     
 }
 fn get_stdin_data() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let mut buf = Vec::new();
-    stdin().read_to_end(&mut buf)?;
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer)?;
+    let buf = buffer.as_bytes().to_vec();
     Ok(buf)
 }
 
@@ -90,14 +91,13 @@ async fn client_mode(server_ip: &str, port: u16) -> Result<(), Box<dyn Error>> {
     let socket = UdpSocket::bind(local_addr).await?;
     const MAX_DATAGRAM_SIZE: usize = 65_507;
     socket.connect(remote_server).await?;
-    let data = get_stdin_data()?;
-    println!("Sending data:\n{}", String::from_utf8_lossy(&data));
-    socket.send(&data).await?;
-    let mut data = vec![0u8; MAX_DATAGRAM_SIZE];
-    let len = socket.recv(&mut data).await?;
-    println!("Received {} bytes:\n{}",len,String::from_utf8_lossy(&data[..len]));
-
-    Ok(())
+    loop {
+        let data = get_stdin_data()?;
+        socket.send(&data).await?;
+        let mut data = vec![0u8; MAX_DATAGRAM_SIZE];
+        let len = socket.recv(&mut data).await?;
+        print!("-> {}",String::from_utf8_lossy(&data[..len]));
+    }
 
 }
 
