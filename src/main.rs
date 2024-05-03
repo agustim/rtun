ede5fff6f328b5b9fd8cf365f02b8cc31ddd3507
Agustí Moll <agusti@gmail.com>
2024-05-03 08:28:28 +0200

    add tun's ecosystem

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use clap::{Parser, ValueEnum};
-use tokio::net::UdpSocket;
-use std::net::{IpAddr, SocketAddr};
 use std::error::Error;
+use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
-use std::io::stdin;
-
+use tokio::net::UdpSocket;
+use env_logger::Builder;
+use log::{debug, error, info, LevelFilter};
+use ipnet::Ipv4Net;
+use std::net::Ipv4Addr;
+use tokio_tun::Tun;
 
 const UDP_BUFFER_SIZE: usize = 17480; // 17kb
 
@@ -20,6 +23,8 @@ struct Cli {
     host: String,
     #[arg(short, long, default_value = "rtun0")]
     iface: String,
+    #[arg(short, long, default_value = "10.9.0.1/24")]
+    address: String,
 }
 #[derive(Clone, ValueEnum)]
 enum Mode {
@@ -27,57 +32,88 @@ enum Mode {
     Client,
 }
 
-struct Server {
-    socket: UdpSocket,
-    buf: Vec<u8>,
-    to_send: Option<(usize, SocketAddr)>,
+async fn create_tun(ipv4: &str, mtu: i32) -> tokio_tun::Tun {
+    let net: Ipv4Net = ipv4.parse().unwrap();
+
+    let tun = Tun::builder()
+        .name("")
+        .tap(false)
+        .packet_info(false)
+        .mtu(mtu)
+        .up()
+        .address(net.addr())
+        .broadcast(Ipv4Addr::BROADCAST)
+        .netmask(net.netmask())
+        .try_build()
+        .unwrap();
+    info!("Tun interface created: {:?}, with IP {}", tun.name(), ipv4);
+    return tun;
 }
 
-impl Server {
+struct Node {
+    socket: UdpSocket,
+    tun: tokio_tun::Tun,
+    buf: Vec<u8>,
+    peer: SocketAddr,  // Així no podem tenir més d'un client, s'hauria de canviar per un HashMap
+    to_send_tun: Option<(usize, SocketAddr)>,
+    recv_from_tun: Option<usize>
+}
+
+impl Node {
     async fn run(self) -> Result<(), Box<dyn Error>> {
-        let Server {
+        let Node {
             socket,
+            tun,
+            mut peer,
             mut buf,
-            mut to_send,
+            mut to_send_tun,
+            mut recv_from_tun,
         } = self;
 
         loop {
-            if let Some((size, peer)) = to_send {
-                let amt = socket.send_to(&buf[..size], &peer).await?;
+            if let Some((size, peer_connect)) = to_send_tun {
 
-                print!("Echoed {}/{} bytes to {} -> {}", amt, size, peer, String::from_utf8_lossy(&buf[..size]));
+                debug!("Received {}/{} bytes from {}", size, UDP_BUFFER_SIZE, peer_connect);
+                peer = peer_connect;
+                tun.send(&buf[..size]).await?;
+            }
+            
+            if let Some(size) = recv_from_tun {
+                debug!("Received {}/{} bytes ", size, UDP_BUFFER_SIZE);
+                
+                let _ = socket.send_to(&buf[..size], &peer).await?;
+
             }
 
-            to_send = Some(socket.recv_from(&mut buf).await?);
+            to_send_tun = Some(socket.recv_from(&mut buf).await?);
+
+            recv_from_tun = Some(tun.recv(&mut buf).await.unwrap());
+
         }
     }
 }
 
 async fn server_mode(port: u16) -> Result<(), Box<dyn Error>> {
-
     let localhost_ip = IpAddr::from_str("127.0.0.1").unwrap();
     let server = SocketAddr::new(localhost_ip, port);
     let socket = UdpSocket::bind(server).await?;
+    let tun = create_tun("10.9.0.1/24", 1500).await;
 
-    let server = Server {
+    let server = Node {
         socket,
+        tun,
+        peer: SocketAddr::new(localhost_ip, 0),
         buf: vec![0; UDP_BUFFER_SIZE],
-        to_send: None,
+        to_send_tun: None,
+        recv_from_tun: None,
     };
-        server.run().await?;
+    server.run().await?;
 
     Ok(())
-    
 }
-fn get_stdin_data() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let mut buffer = String::new();
-    stdin().read_line(&mut buffer)?;
-    let buf = buffer.as_bytes().to_vec();
-    Ok(buf)
-}
+
 
 async fn client_mode(server_ip: &str, port: u16) -> Result<(), Box<dyn Error>> {
-
     let remote_addr = IpAddr::from_str(server_ip).unwrap();
     let remote_server = SocketAddr::new(remote_addr, port);
 
@@ -89,33 +125,42 @@ async fn client_mode(server_ip: &str, port: u16) -> Result<(), Box<dyn Error>> {
     .parse()?;
 
     let socket = UdpSocket::bind(local_addr).await?;
-    const MAX_DATAGRAM_SIZE: usize = 65_507;
     socket.connect(remote_server).await?;
-    loop {
-        let data = get_stdin_data()?;
-        socket.send(&data).await?;
-        let mut data = vec![0u8; MAX_DATAGRAM_SIZE];
-        let len = socket.recv(&mut data).await?;
-        print!("-> {}",String::from_utf8_lossy(&data[..len]));
-    }
 
+    let tun = create_tun("10.9.0.2/24", 1500).await;
+
+    let client = Node {
+        socket,
+        tun,
+        peer: remote_server,
+        buf: vec![0; UDP_BUFFER_SIZE],
+        to_send_tun: None,
+        recv_from_tun: None,
+    };
+    client.run().await?;
+
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
+    Builder::new()
+        .filter(None, LevelFilter::Info)
+        .init();
+
     let cli = Cli::parse();
 
     match cli.mode {
         Mode::Server => {
-            println!("server in port {} started", cli.port);
+            info!("server in port {} started", cli.port);
             let _ = server_mode(cli.port).await;
         }
         Mode::Client => {
             if cli.host.is_empty() {
-                eprintln!("host is required for client mode");
+                error!("host is required for client mode");
                 std::process::exit(1);
             }
-            println!("client, connect to {}:{}", cli.host, cli.port);
+            info!("client, connect to {}:{}", cli.host, cli.port);
             let _ = client_mode(&cli.host, cli.port).await;
         }
     }
